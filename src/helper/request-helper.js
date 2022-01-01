bedc03063828d987e872b593ef3fed85e48fcd17
Dima <omeluan.dima@gmail.com>
2022-01-01 22:15:49 +0200

    Fix bugs in offline mode

--- src/helper/request-helper.js
+++ src/helper/request-helper.js
@@ -1,58 +1,58 @@
 class RequestHelper {
-    API_URL = "https://web-laba3-2.herokuapp.com/v1/graphql";
+  API_URL = "https://web-laba3-2.herokuapp.com/v1/graphql";
 
-    async fetchGraphQL(operationsDoc, operationName, variables) {
-      try {
-        const result = await fetch(this.API_URL, {
-          method: "POST",
-          body: JSON.stringify({
-            query: operationsDoc,
-            variables: variables,
-            operationName: operationName,
-          }),
-        });
+  async fetchGraphQL(operationsDoc, operationName, variables) {
+    try {
+      const result = await fetch(this.API_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          query: operationsDoc,
+          variables: variables,
+          operationName: operationName,
+        }),
+      });
 
-        return await result.json();
-      } catch (e) {
-        console.log(e)
+      return await result.json();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  fetchMyQuery(operationsDoc) {
+    return this.fetchGraphQL(operationsDoc, "MyQuery", {});
+  }
+
+  async startFetchMyQuery(operationsDoc) {
+    try {
+      const { errors, data } = await this.fetchMyQuery(operationsDoc);
+
+      if (errors) {
+        console.error(errors);
       }
+      console.log(data);
+      return data;
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    fetchMyQuery(operationsDoc) {
-        return this.fetchGraphQL(operationsDoc, "MyQuery", {});
-    }
+  executeMyMutation(operationsDoc) {
+    return this.fetchGraphQL(operationsDoc, "MyMutation", {});
+  }
 
-    async startFetchMyQuery(operationsDoc) {
-      try {
-        const {errors, data} = await this.fetchMyQuery(operationsDoc);
+  async startExecuteMyMutation(operationsDoc) {
+    try {
+      const { errors, data } = await this.executeMyMutation(operationsDoc);
 
-        if (errors) {
-          console.error(errors);
-        }
-        console.log(data);
-        return data;
-      } catch (e) {
-        console.log(e)
+      if (errors) {
+        console.error(errors);
       }
+      console.log(data);
+      return data;
+    } catch (e) {
+      console.log(e);
     }
-
-    executeMyMutation(operationsDoc) {
-        return this.fetchGraphQL(operationsDoc, "MyMutation", {});
-    }
-
-    async startExecuteMyMutation(operationsDoc) {
-      try {
-        const {errors, data} = await this.executeMyMutation(operationsDoc);
-
-        if (errors) {
-          console.error(errors);
-        }
-        console.log(data);
-        return data;
-      } catch (e) {
-        console.log(e)
-      }
-    }
+  }
 }
 
 export default new RequestHelper();
