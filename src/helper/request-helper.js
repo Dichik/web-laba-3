3a9677093be0170e5d35943fd3f5d87e1363f0be
Dima <omeluan.dima@gmail.com>
2022-01-02 09:28:20 +0200

    tech: updates and fixing

--- src/helper/request-helper.js
+++ src/helper/request-helper.js
@@ -1,5 +1,5 @@
 class RequestHelper {
-  API_URL = "https://web-laba3-2.herokuapp.com/v1/graphql";
+  API_URL = _API_URL;
 
   async fetchGraphQL(operationsDoc, operationName, variables) {
     try {
