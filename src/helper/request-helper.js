bcc5386376a86d69be1581da4c1080d8ce6b625e
Dima <omeluan.dima@gmail.com>
2022-01-14 20:51:59 +0200

    tech

--- src/helper/request-helper.js
+++ src/helper/request-helper.js
@@ -1,48 +1,48 @@
 class RequestHelper {
-    API_URL = _api_url;
+  API_URL = _api_url;
 
-    async fetchGraphQL(operationsDoc, operationName, variables) {
-        const result = await fetch(this.API_URL, {
-            method: "POST",
-            body: JSON.stringify({
-                query: operationsDoc,
-                variables: variables,
-                operationName: operationName,
-            }),
-        });
+  async fetchGraphQL(operationsDoc, operationName, variables) {
+    const result = await fetch(this.API_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName,
+      }),
+    });
 
-        return await result.json();
+    return await result.json();
+  }
+
+  fetchMyQuery(operationsDoc) {
+    return this.fetchGraphQL(operationsDoc, "MyQuery", {});
+  }
+
+  async startFetchMyQuery(operationsDoc) {
+    const { errors, data } = await this.fetchMyQuery(operationsDoc);
+
+    if (errors) {
+      console.error(errors);
+      return;
     }
+    console.log(data);
+    return data;
+  }
 
-    fetchMyQuery(operationsDoc) {
-        return this.fetchGraphQL(operationsDoc, "MyQuery", {});
+  executeMyMutation(operationsDoc) {
+    return this.fetchGraphQL(operationsDoc, "MyMutation", {});
+  }
+
+  async startExecuteMyMutation(operationsDoc) {
+    const { errors, data } = await this.executeMyMutation(operationsDoc);
+
+    if (errors) {
+      console.error(errors);
+      return;
     }
-
-    async startFetchMyQuery(operationsDoc) {
-        const {errors, data} = await this.fetchMyQuery(operationsDoc);
-
-        if (errors) {
-            console.error(errors);
-            return;
-        }
-        console.log(data);
-        return data;
-    }
-
-    executeMyMutation(operationsDoc) {
-        return this.fetchGraphQL(operationsDoc, "MyMutation", {});
-    }
-
-    async startExecuteMyMutation(operationsDoc) {
-        const {errors, data} = await this.executeMyMutation(operationsDoc);
-
-        if (errors) {
-            console.error(errors);
-            return;
-        }
-        console.log(data);
-        return data;
-    }
+    console.log(data);
+    return data;
+  }
 }
 
 export default new RequestHelper();
