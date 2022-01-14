fae40ae88886866c6f1545fd4a1a04b15fc4b134
Dima <omeluan.dima@gmail.com>
2022-01-14 20:50:22 +0200

    Replace Prompts, fix exceptions and add Modal

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,17 @@
     import {setClient, subscribe} from "svelte-apollo";
     import {WebSocketLink} from "@apollo/client/link/ws";
     import './styles/global.css'
+    import Modal, {bind} from 'svelte-simple-modal'
+    import {modal} from './store'
+    import MessageBox from './componenets/Message.svelte'
 
     let uri_link = _uri_apollo_client
+
+    let defaultValue = ""
+
+    let name = defaultValue
+    let priority = defaultValue
+    let deadline = defaultValue
 
     function createApolloClient() {
         try {
@@ -22,12 +31,11 @@
                 cache
             })
         } catch (e) {
-            console.log(ERROR_MESSAGE + e)
+            modal.set(bind(MessageBox, { message: ("Error: " + e)}));
         }
     }
 
     const ERROR_MESSAGE = 'Something went wrong... see exception: ';
-    let deadlineError = false
     let client;
     let tasks;
     try {
@@ -35,70 +43,66 @@
         setClient(client)
         tasks = subscribe(OperationDocsHelper.SUBSCRIPTION_AllTodos)
     } catch (e) {
-        console.log(ERROR_MESSAGE + e)
+        modal.set(bind(MessageBox, { message: ("Error: " + e)}));
     }
 
     const convertToNumber = (string) => {
         return isNaN(+string) ? 0 : +string;
     }
-    // prettier-ignore
-    // TODO add update
-
-    // prettier-ignore
-    // TODO add search (search by)
-
-    // prettier-ignore
-    // TODO pagination
 
     const addTask = async () => {
-        // prettier-ignore
-        // FIXME use smth instead of prompts
 
         if(!window.navigator.onLine) {
-            alert('Ooops, something went wrong, please check your connection.')
+            modal.set(bind(MessageBox, { message: ('Ooops, something went wrong, please check your connection.')}));
             return
         }
 
-        const name = prompt("Task name: ") ?? "";
-        const priority = convertToNumber(prompt("Task priority: ") ?? "");
-        const deadline = prompt("Deadline: ") ?? "";
+        // const name = prompt("Task name: ") ?? "";
+        // const priority = convertToNumber(prompt("Task priority: ") ?? "");
+        // const deadline = prompt("Deadline: ") ?? "";
 
-        if(!name.trim() || !deadline.trim() || !deadline.trim()) {
-            alert("You entered empty string(s)... or there is some troubles, try again")
+        if(!name.trim() || !deadline.trim()) {
+            modal.set(bind(MessageBox, { message: ("You entered empty string(s)... or there is some troubles, try again")}));
             return
         }
 
         if(Date.parse(deadline) < Date.now()) {
-            alert("can't we live past... but future we can change, man")
+            modal.set(bind(MessageBox, { message: ("can't we live past... but future we can change, man")}));
             return
         }
 
         try {
             await http.startExecuteMyMutation(OperationDocsHelper.MUTATION_InsertOne(name, priority, deadline));
+            modal.set(bind(MessageBox, { message: ("Success!")}));
+            backToDefault();
         } catch (e) {
-            console.log(ERROR_MESSAGE + e);
-            alert('Ooops, something went wrong, please check your connection.')
+            modal.set(bind(MessageBox, { message: ('Ooops, something went wrong, please check your connection.' + e)}));
         }
+    }
+
+    const backToDefault = () => {
+        name = defaultValue
+        priority = defaultValue
+        deadline = defaultValue
     }
 
     const deleteTasks = async () => {
         if(!window.navigator.onLine) {
-            alert('Ooops, something went wrong, please check your connection.')
+            modal.set(bind(MessageBox, { message: ('Ooops, something went wrong, please check your connection.')}));
             return
         }
 
         try {
             await http.startExecuteMyMutation(OperationDocsHelper.DELETE_DONE_TASKS());
-            // open(MessageBox, {modalText: "Success!"})
+            modal.set(bind(MessageBox, { message: ("Success!")}));
         } catch (e) {
-            console.log(ERROR_MESSAGE + e);
-            alert('Ooops, something went wrong, please check your connection.')
+            modal.set(bind(MessageBox, { message: ("Error: " + e)}));
         }
     }
 
     const getDoneFromId = async (id) => {
         if(!window.navigator.onLine) {
-            alert('Ooops, something went wrong, please check your connection.')
+            modal.set(bind(MessageBox, { message: ('Ooops, something went wrong, please check your connection.')}));
             return
         }
 
@@ -107,13 +111,13 @@
             if(train_todolist.length !== 0)
                 return train_todolist[0].done;
         } catch (e) {
-            console.log(ERROR_MESSAGE + e)
+            throw ERROR_MESSAGE + " : " + e;
         }
     }
 
     const markTask = async (id) => {
         if(!window.navigator.onLine) {
-            alert('Ooops, something went wrong, please check your connection.')
+            modal.set(bind(MessageBox, { message: ('Ooops, something went wrong, please check your connection.')}));
             return
         }
 
@@ -121,7 +125,8 @@
             let flag = await getDoneFromId(id)
             await http.startExecuteMyMutation(OperationDocsHelper.UPDATE_DONE(id, !flag))
         } catch (e) {
-            console.log(e)
+            modal.set(bind(MessageBox, { message: ('Ooops, something went wrong, please check your connection. See error: ' + e)}));
+
         }
     }
 
@@ -129,50 +134,66 @@
 </script>
 
 <main class="main-style">
-    {#if $tasks.loading}
+    <Modal show={$modal}>
+        {#if $tasks.loading}
         <div class="centerize">
             <h1>Loading...</h1>
         </div>
-    {:else if $tasks.error}
-        <div>Error!</div>
-    {:else if $tasks.data}
-        <button class="button-style" on:click={addTask}>Add task</button>
-        <button class="button-style" on:click={deleteTasks}>Delete Done</button>
-        <button class="button-style">Update Task</button>
-        <table border="1" class="table-centerize">
-            <caption class="table-global-style">Tasks</caption>
-            <tr class="table-global-style">
-                <th></th>
-                <th>Task</th>
-                <th>Priority</th>
-                <th>Deadline</th>
-                <th>Done</th>
-            </tr>
-            {#each $tasks.data.train_todolist as t (t.id)}
-                {#if t.done}
-                    <tr class="table-global-style rows-style-done">
-                        <td>
-                            <input type="checkbox" checked={t.done} name="done" on:click={markTask(t.id)} />
-                        </td>
-                        <td><strike>{t.task}</strike></td>
-                        <td><strike>{t.priority}</strike></td>
-                        <td><strike>{t.deadline}</strike></td>
-                        <td><strike>{t.done}</strike></td>
-                    </tr>
-                {:else }
-                    <tr class="table-global-style rows-style-notdone">
-                        <td>
-                            <input type="checkbox" checked={t.done} name="done" on:click={markTask(t.id)} />
-                        </td>
-                        <td>{t.task}</td>
-                        <td>{t.priority}</td>
-                        <td>{t.deadline}</td>
-                        <td>{t.done}</td>
-                    </tr>
-                {/if }
-            {/each}
-        </table>
-    {/if}
+        {:else if $tasks.error}
+            <div>Error!</div>
+        {:else if $tasks.data}
+            <button class="button-style" on:click={addTask}>Add task</button>
+            <button class="button-style" on:click={deleteTasks}>Delete Done</button>
+            <button class="button-style">Update Task</button>
+
+            <div class="input-fields">
+                <label>
+                    Task Name <input bind:value={name} class="input-field-style">
+                </label>
+                <label>
+                    Priority <input bind:value={priority} class="input-field-style">
+                </label>
+                <label>
+                    Deadline <input bind:value={deadline} class="input-field-style">
+                </label>
+            </div>
+
+            <table border="1" class="table-centerize">
+                <caption class="table-global-style">Tasks</caption>
+                <tr class="table-global-style">
+                    <th></th>
+                    <th>Task</th>
+                    <th>Priority</th>
+                    <th>Deadline</th>
+                    <th>Done</th>
+                </tr>
+                {#each $tasks.data.train_todolist as t (t.id)}
+                    {#if t.done}
+                        <tr class="table-global-style rows-style-done">
+                            <td>
+                                <input type="checkbox" checked={t.done} name="done" on:click={markTask(t.id)} />
+                            </td>
+                            <td><strike>{t.task}</strike></td>
+                            <td><strike>{t.priority}</strike></td>
+                            <td><strike>{t.deadline}</strike></td>
+                            <td><strike>{t.done}</strike></td>
+                        </tr>
+                    {:else }
+                        <tr class="table-global-style rows-style-notdone">
+                            <td>
+                                <input type="checkbox" checked={t.done} name="done" on:click={markTask(t.id)} />
+                            </td>
+                            <td>{t.task}</td>
+                            <td>{t.priority}</td>
+                            <td>{t.deadline}</td>
+                            <td>{t.done}</td>
+                        </tr>
+                    {/if }
+                {/each}
+            </table>
+        {/if}
+    </Modal>
+    
 </main>
 
 <style>
--- src/helper/request-helper.js
+++ src/helper/request-helper.js
@@ -1,58 +1,48 @@
 class RequestHelper {
-  API_URL = _api_url;
+    API_URL = _api_url;
 
-  async fetchGraphQL(operationsDoc, operationName, variables) {
-    try {
-      const result = await fetch(this.API_URL, {
-        method: "POST",
-        body: JSON.stringify({
-          query: operationsDoc,
-          variables: variables,
-          operationName: operationName,
-        }),
-      });
+    async fetchGraphQL(operationsDoc, operationName, variables) {
+        const result = await fetch(this.API_URL, {
+            method: "POST",
+            body: JSON.stringify({
+                query: operationsDoc,
+                variables: variables,
+                operationName: operationName,
+            }),
+        });
 
-      return await result.json();
-    } catch (e) {
-      console.log(e);
+        return await result.json();
     }
-  }
 
-  fetchMyQuery(operationsDoc) {
-    return this.fetchGraphQL(operationsDoc, "MyQuery", {});
-  }
-
-  async startFetchMyQuery(operationsDoc) {
-    try {
-      const { errors, data } = await this.fetchMyQuery(operationsDoc);
-
-      if (errors) {
-        console.error(errors);
-      }
-      console.log(data);
-      return data;
-    } catch (e) {
-      console.log(e);
+    fetchMyQuery(operationsDoc) {
+        return this.fetchGraphQL(operationsDoc, "MyQuery", {});
     }
-  }
 
-  executeMyMutation(operationsDoc) {
-    return this.fetchGraphQL(operationsDoc, "MyMutation", {});
-  }
+    async startFetchMyQuery(operationsDoc) {
+        const {errors, data} = await this.fetchMyQuery(operationsDoc);
 
-  async startExecuteMyMutation(operationsDoc) {
-    try {
-      const { errors, data } = await this.executeMyMutation(operationsDoc);
-
-      if (errors) {
-        console.error(errors);
-      }
-      console.log(data);
-      return data;
-    } catch (e) {
-      console.log(e);
+        if (errors) {
+            console.error(errors);
+            return;
+        }
+        console.log(data);
+        return data;
     }
-  }
+
+    executeMyMutation(operationsDoc) {
+        return this.fetchGraphQL(operationsDoc, "MyMutation", {});
+    }
+
+    async startExecuteMyMutation(operationsDoc) {
+        const {errors, data} = await this.executeMyMutation(operationsDoc);
+
+        if (errors) {
+            console.error(errors);
+            return;
+        }
+        console.log(data);
+        return data;
+    }
 }
 
 export default new RequestHelper();
